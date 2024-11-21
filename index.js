119560881ba46dee4d17aa3a45f0abcbf11d3f87
Tarun Tailor <taruntailor7@gmail.com>
2024-11-21 23:35:01 +0530

    feat: delete add update mutations added

--- index.js
+++ index.js
@@ -43,6 +43,31 @@ const resolvers = {
         game (parent) {
             return db.games.find((g) => g.id === parent.game_id);
         }
+    },
+    Mutation: {
+        deleteGame(_, args) {
+            db.games = db.games.filter((game) => game.id !== args.id);
+            return db.games;
+        },
+        addGame(_, args) {
+            let game = {
+                ...args.game,
+                id: Math.floor(Math.random() * 10000).toString()
+            }
+            db.games.push(game);
+            return game;
+        },
+        updateGame(_, args) {
+            db.games = db.games.map((game) => {
+                if(game.id === args.id){
+                    return {...game, ...args.edits}
+                }
+
+                return game;
+            })
+
+            return db.games.find((game) => game.id === args.id);
+        }
     }
 }
 
